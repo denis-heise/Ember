fbfc24c230e3eededfe57d98fbacce13605a80d3
Denis Heise <denis.belokopytov.v@gmail.com>
2022-08-14 12:50:53 +0300

    отредактировал ссылки

--- app/router.js
+++ app/router.js
@@ -10,6 +10,10 @@ Router.map(function() {
   this.route('meetings');
   this.route('books');
   this.route('speakers');
+  this.route('edit-book', { path: 'books/edit-book'});
+  this.route('edit-speaker', { path: 'speakers/edit-speaker'});
+  this.route('edit-meeting', { path: 'meetings/edit-meeting'});
+  this.route('edit-report', { path: 'meetings/edit-meeting/edit-report'});
 });
 
 export default Router;
